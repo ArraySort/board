b0fd316ec3d447d283f4646ff1efa2b6755d8da8
arraysort <dudwns1045@gmail.com>
2024-07-29 22:29:38 +0900

    feat : UserUtil 로그인 한 유저의 userId 획득 메서드 작성
    1. SpringSecurityContextHolder 를 통한 로그인한 유저의 ID 획득 메서드 작성

--- src/main/java/arraysort/project/board/app/utils/UserUtil.java
+++ src/main/java/arraysort/project/board/app/utils/UserUtil.java
@@ -1,12 +1,27 @@
 package arraysort.project.board.app.utils;
 
+import arraysort.project.board.app.exception.InvalidPrincipalException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserUtil {
 
     private UserUtil() {
         throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * 로그인 한 유저의 Username(userId) 를 가져옴
+     *
+     * @return 로그인 한 유저의 userId
+     */
+    public static String getCurrentLoginUserId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal == null) throw new InvalidPrincipalException();
+
+        return ((UserDetails) principal).getUsername();
     }
 
     /**
