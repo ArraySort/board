cab0496a808935b6862edfdf54524a85430292f1
arraysort <dudwns1045@gmail.com>
2024-08-06 20:25:02 +0900

    chore : 카테고리 Mapper 이름 수정

--- src/main/java/arraysort/project/board/app/category/mapper/CategoryMapper.java
+++ src/main/java/arraysort/project/board/app/category/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package arraysort.project.board.app.category.mapper;
 import arraysort.project.board.app.category.domain.CategoryVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -10,4 +11,7 @@ public interface CategoryMapper {
 
 	// 카테고리 세부내용 조회
 	Optional<CategoryVO> selectCategoryDetailById(long categoryId);
+
+	// 카테고리 목록 조회
+	List<CategoryVO> selectCategoryListByBoardId(long boardId);
 }
