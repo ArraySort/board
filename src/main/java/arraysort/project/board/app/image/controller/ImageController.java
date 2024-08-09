6dcf33310f0fdd329a833fa3a46b0e2d81116197
arraysort <dudwns1045@gmail.com>
2024-08-09 21:15:11 +0900

    feat : 이미지 세부 조회 시 예외처리 추가
    1. ImageController 이미지 세부 조회 시 IOException 예외처리 추가

--- src/main/java/arraysort/project/board/app/image/controller/ImageController.java
+++ src/main/java/arraysort/project/board/app/image/controller/ImageController.java
@@ -3,6 +3,8 @@ package arraysort.project.board.app.image.controller;
 import arraysort.project.board.app.image.domain.ImageVO;
 import arraysort.project.board.app.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Controller
+@Slf4j
 @RequiredArgsConstructor
 public class ImageController {
 
@@ -21,12 +24,17 @@ public class ImageController {
 
 	// 게시글 세부조회에서 이미지 링크 클릭 시
 	@GetMapping("/image/{imageId}")
-	public ResponseEntity<byte[]> getImage(@PathVariable long imageId) throws IOException {
+	public ResponseEntity<byte[]> getImage(@PathVariable long imageId) {
 		ImageVO vo = imageService.findImageById(imageId);
-		byte[] imageBytes = Files.readAllBytes(Paths.get(vo.getImagePath()));
 
-		return ResponseEntity.ok()
-				.contentType(MediaType.IMAGE_PNG)        // TODO : 이미지 타입 분리
-				.body(imageBytes);
+		try {
+			byte[] imageBytes = Files.readAllBytes(Paths.get(vo.getImagePath()));
+			return ResponseEntity.ok()
+					.contentType(MediaType.IMAGE_PNG)
+					.body(imageBytes);
+		} catch (IOException e) {
+			log.error("[ 이미지 업로드 에러 : ]", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 	}
 }
