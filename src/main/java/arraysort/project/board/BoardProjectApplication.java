03db2c596bc072cc0b14e331ef5f417edabbe6f0
arraysort <dudwns1045@gmail.com>
2024-09-20 22:59:35 +0900

    build : 프로젝트 application 클래스 변경
    1. 프로젝트 application 클래스 변경
     - war 빌드에 따른 SpringBootServletInitializer 병합

--- src/main/java/arraysort/project/board/BoardProjectApplication.java
+++ src/main/java/arraysort/project/board/BoardProjectApplication.java
@@ -3,11 +3,18 @@ package arraysort.project.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class BoardProjectApplication {
+public class BoardProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardProjectApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoardProjectApplication.class);
 	}
 }
